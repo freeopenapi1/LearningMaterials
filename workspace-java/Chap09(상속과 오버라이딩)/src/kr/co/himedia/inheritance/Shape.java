69eaa0c0cd55d2dab6543dccb4f72c1ab2fd60aa
freeopenapi1 <138086438+freeopenapi1@users.noreply.github.com>
2023-07-18 18:22:33 +0900

    7/18

--- workspace-java/Chap09(상속과 오버라이딩)/src/kr/co/himedia/inheritance/Shape.java
+++ workspace-java/Chap09(상속과 오버라이딩)/src/kr/co/himedia/inheritance/Shape.java
@@ -3,5 +3,13 @@ package kr.co.himedia.inheritance;
 //조상클래스
 public class Shape {
 
+	String color = "black";
 	
+	public Shape() {
+		System.out.println("Shape 클래스의 기본 생성자 호출");
+	}
+	
+	public void draw() {
+		System.out.println("draw()");
+	}
 }
---
+++ workspace-java/Chap09(상속과 오버라이딩)/src/kr/co/himedia/inheritance/ShapeTest.java
@@ -0,0 +1,84 @@
+package kr.co.himedia.inheritance;
+
+public class ShapeTest {
+
+	public static void main(String[] args) {
+		
+		Circle circle = new Circle();
+		circle.draw();
+		System.out.println();
+		
+		Circle circle2 = new Circle(new Point(150, 150), 500);
+		circle2.draw();
+		System.out.println();
+		
+		Triangle triangle = new Triangle();
+		triangle.draw();
+		System.out.println();
+		
+		Triangle triangle2 = 
+				new Triangle(
+						new Point(10, 10), new Point(20, 20), new Point(30, 30));	
+		triangle2.draw();
+		System.out.println();
+		
+		
+		Point[] points = 
+				new Point[] {
+						new Point(10,10), new Point(80,100), new Point(20,120)};
+		
+		Triangle triangle3 = new Triangle(points);
+		triangle3.draw();
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
